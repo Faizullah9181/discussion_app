122e0c2337385e66917617f24917546533583f39
FAIZULLAH <faizullah9181@gmail.com>
2022-10-01 12:41:48 +0530

    hosts

--- dicussion_app/settings.py
+++ dicussion_app/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '&qi#k!pgp8n%z#lb&62d2^5=5h!%l4uz584qet#8+-(z)9$k@b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
