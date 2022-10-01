d9c15ea1d01e47bf225b01a75f2c96156599cd3c
FAIZULLAH <faizullah9181@gmail.com>
2022-10-01 13:08:19 +0530

    database

--- dicussion_app/settings.py
+++ dicussion_app/settings.py
@@ -87,7 +87,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -119,10 +119,15 @@ WSGI_APPLICATION = 'dicussion_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'cPjY5htUu6t0iGnSH6hF',
+        'HOST': 'containers-us-west-73.railway.app',
+        'PORT': '6582',
     }
 }
+
 
 
 # Password validation
