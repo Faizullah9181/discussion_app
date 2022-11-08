57b0c2523d080bbb1d78ac9824a50f5e57320ac4
FAIZULLAH <faizullah9181@gmail.com>
2022-11-08 13:23:56 +0530

    .

--- dicussion_app/settings.py
+++ dicussion_app/settings.py
@@ -168,23 +168,23 @@ WSGI_APPLICATION = 'dicussion_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'railway',
-#         'USER': 'postgres',
-#         'PASSWORD': 'BCZkSJNH4OI9eqthzacG',
-#         'HOST': 'containers-us-west-101.railway.app',
-#         'PORT': '7588',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'BCZkSJNH4OI9eqthzacG',
+        'HOST': 'containers-us-west-101.railway.app',
+        'PORT': '7588',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 # DATABASES = {
 #     'default': { 
