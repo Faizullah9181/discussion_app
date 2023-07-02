ad093b670e0d4607d17f282e6c04c31925ec1bae
FAIZULLAH <faizullah9181@gmail.com>
2023-07-02 17:43:52 +0530

    fixing noti

--- postapp/utils.py
+++ postapp/utils.py
@@ -5,17 +5,21 @@ from django.conf import settings
 import json
 
 exmp = {
+
     "type": "service_account",
-    "project_id": "discussion-noti-94921",
-    "private_key_id": "89a5322b6f65228b3b7b726e58268267a2645667",
-    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQDIQ2LpBl4VlkbM\n4XL76jE2gT+o0fkPtusfpfOVlDNztyCeKtCLDQ+zp2orDCz75hH00/yzGd9lUXxk\nlNfhpgZXV9MZYnXTkgIk9M9h1DGjDcLuSsQava37j1CFuddVrkG6M/tPsWjT7RAI\nE6YhJ25mR4wN2f9iPVHgMbc9zR/mNVKVR5VJGJI2+5hQ0NIKRgku+lPnF4tyEMV9\nMEgoLgaxBXXjcpowRFz3D65vv+De6j+dJQu8gtQf1XkwNVDDMsXx9sr8iY89XtYa\n6yvjeBZRa8sM8aAKYtu8mwgtuhYfT2Tgp//PvrNQ+bFeqXO3KnGw/F8gZICx3VhN\nhVm29osHAgMBAAECggEAPe3MyAfej2ll4pSzVKDC8Dp+36tzDD+CLUrJ7U8jrByM\n2yydSerTmLhkVgSRYqJumhn2EsKk+TV+NLJsogyP+HGhx79RdYg43onQGc4fPW+T\ngNIhG63dgCX8/iWOqCHXlIffpvDTXHn6U60T2jvR4ZIthtkLpysURzLImjV1MGBv\n4E2zQ2QAl6E3Y3LNrfkQsvuo1UnJ/RrCEZUri7BDVP27aFQ7mH+83G6DqutSxlwi\nkTsFjKaDzraV4rMZly10+iyy4yPn7j+jRoxN4CFcGrofWISUR2iJ5jPgWPj4klA1\nA20CMGRmSvsa4g4heCbWoSFdQLaadkUHXJ0NZ100CQKBgQDoPGsK2QMuo8beNvra\nxz3f8YyNXNxRcxDPw5qsBQsJhnX3f9MXFYRonNWOYlcgPnOw1JRG8gBgxdscBbS3\n6AKFT5J7J5EOAI08P7wa+nBzZ4WQN8EvSA8NsJuI6Nk/AzSQZ3ZGsB2Yw75AEYj+\nJkQdTLkdBZ1YifDvZjlCSLeLnwKBgQDcwWtVtKn2iWMisguPe5ZPP4Itk8eET0PG\n9Uj4zHq8Em1INgo2gMcs3usLFaoC91HYPQdLaTnRpoIlnvQ5ToClFg03MiNoTpc1\n8yoCDmXPFee+pIH2KzRHU7EmA/djJBp8G1cuUlGy2Nph6FUE9RJHLecBE2JIyqBA\nb6ULKpVHmQKBgQDIRPH6VNKNseaZUshZSmBtcpxGe8Hy60ugH4qOtXqjKDUqzix3\nR7L7k+QF+YAw6ipSX7/Ovnrc+ULOk40Y2xbxZ4FmeuR4MrmZL01D5px6qNdhWvNZ\nlsaEg7xdAI/36we1T8OLiey8BHFYRsbghJdQvQ0VnfXmmG4RxRGzu4ms+QKBgQDQ\nr8BKGyn5N9ORvwKSt2BWtlDXyTX5a/1EnihHmvYDyN+qPfvVnlOrpWhI6Ef52ddz\nNe4paeYQvPmXmvaBHica6QfBQBKczn6/r4nsu7Oe2rRjUpzRG/Kl4wY6S9GX1U98\nN1RpI8S776gymjGVKCAJTels6Ogmq/ZmM6SwM6c/MQKBgQDVeGinM4Q0veQn5NQB\nA3lLgZveevlxJu3srR7rCpwvRc2Fsdpfz8Hi44GsxYWV1MSsYOwsAXmW6nGE560q\npEIKEtJLOA0WC0oonWh8jum9+4Tau2ZOVKoewOMHqAoCA6qSCL/PhmU7SWMUZbMa\nQv+PI5aLZJwKubgvn8s0rLxQNA==\n-----END PRIVATE KEY-----\n",
-    "client_email": "firebase-adminsdk-ox85o@discussion-noti-94921.iam.gserviceaccount.com",
-    "client_id": "108762647299071594926",
+    "project_id": "noti-discuss",
+    "private_key_id": "47d754942305792818431607aafa6d25a9cf346b",
+    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQD6tqpt1hhJKysM\noFASIUmVlL3uF4plJZiJC2Gbpetfehp/Z4WnqRzlHmZ8ya4Ktpr9ZshY5WcV625Y\n4RDEaLf1a1dlmrvVF+p0zQ3cWhjR72OC90rM0xNU1pBRe8DkIQxV5D04A9NRlaEs\ncOP/urpwazUQq7zeaIFBxnLpYQ5dmilu3QP2RnLxNVue2mGNe4bm8nZLh50PFIY0\n3raM0MrD4bO0xajLpJpeYohAludsyVKH+9alxdyg5BZR8l35Mac64PEI0CHGXLRC\nGhCjQuGJ/rsA9AQpg5MFdZqbwXWeJG6T/rIEbHkJHnhiItxyKJOc5h9yUEq1H3Nn\nj4C7P3FbAgMBAAECggEAQkWBtGno3oJyLTK9J6Wz6T25nNONKiJveR5QjU+k8YZ5\ntZ68Ni7eK/RGvAqlnkBZpM0D/XeaMbGbaBQKFhjmM5HNttZZe0EIoRpI5SvNrsSc\nJdTPaWgMDptu55MSUFsqAxRDWGiudbzbLz+iDQmjWa8FBmSXFqVCMYxO+MDGhtgA\n1eZHcxDnzEhlaGoQPljUtNfn6ZxSkYGELjlSVZt8KfbinUn1Z7CWdE4KFNB4KKNp\n5XnOINURpQFFGo9TmV6sbQi1QjeTEUdwVu6pd7ze2NYDKShXJvudm1PZfcGAA2TV\niVNJgUbn7lAfar1xHkRPz/oN7mzU2dyrPQHNg+PKCQKBgQD/sxi533P48rCS2blD\nXETwLAUKIk/IoOeRK+3Hvt+5eBoNBfZi3nn7fEgyji5WIrIE8bIT0HY/7BNI93GE\nQIgVqTG3uiXDgEhUx/12goulX5E8f568HhKtznROzGV3cM18Hl+r0z9nGrd9GgEJ\nQGqnW/IoFJicKK7XygWPfOYV8wKBgQD7AhHOwAVGzyVr5yXaEZpSdmE7G0HU5BmH\nfCPXQeO2REPGRlpfOTg2wwDjrrDvFNL0wq1WptUdCpnXtS/rJCh5cu4mZoL7UU97\nTh5mJiYf/x3ZqnHGhYHs3PVUNaKl55OnklCFR1Q6Rz6XSJ8zSkBodI7ypv7tkn+X\nRKagGMmI+QKBgF3Na+K0MK1BCrm0mZw2eyvIWkjvcWlebdT50o8WXKiDgLsKVurK\nRuGVJD83nZQHwlrB99wA+4BTsHdyHvivW7zmhtCpvzTLGL3RaBBTr7sJJhSVlLva\nJGwScuR9YzO/9AH2eo5kPrwqNunz4jFsqjOfLWS6uYl/+zQ6bnPHE/k1AoGBAMlI\nbZNH9HNF5ZJTt8zZVNFP0MSfJPldOQOmju8tDTe6Ikc7X3kA3406btol2gi+wDQ6\ncNoNytQRDjEmQJjlU/egLE1q4xo5p+0PRd1QNj5uTDeiML6rjPaucaTnpdg4jfVQ\npXVbL7XUL+MM/20X2/JnlcIMHF3SnrJlAOFdPy+xAoGBALw7HF/dpOTw85hcjr40\nMJcqYHl6MMpZuvNxtmwR/OkuqI7yAe13fffXdr02ekKrnhDUU+dglfGi0B53em+8\nbxvZeQyr7o7TjdQlCtDYCqpIYKB+ig7uHV3gD4X73NQbWBialB7fiHn/OefDyoFe\nFPyr6MiicdQxonVwUNOZKHjd\n-----END PRIVATE KEY-----\n",
+    "client_email": "firebase-adminsdk-l41vv@noti-discuss.iam.gserviceaccount.com",
+    "client_id": "104223741460346907036",
     "auth_uri": "https://accounts.google.com/o/oauth2/auth",
     "token_uri": "https://oauth2.googleapis.com/token",
     "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-ox85o%40discussion-noti-94921.iam.gserviceaccount.com"
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-l41vv%40noti-discuss.iam.gserviceaccount.com",
+    "universe_domain": "googleapis.com"
+
 }
+
 
 cred = credentials.Certificate(exmp)
 firebase_admin.initialize_app(cred)
@@ -82,7 +86,7 @@ def send_noti_commets_comments(*values):
             "comment":  str({
                 "id": (values[0].id),
                 "content": (values[0].content),
-                "comment":(values[1].content)
+                "comment": (values[1].content)
             }),
         },
         token=values[2]
@@ -148,8 +152,8 @@ def send_noti_like_comments(*values):
             "comment":  str({
                 "id": (values[1].id),
                 "content": (values[0].content),
-                
-                
+
+
             })
         },
         token=values[2]
